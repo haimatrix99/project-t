439498ad9f96f94ba447886c3c0267f9c1e1c00d
Le Trung Hai <letrunghai.92nl@gmail.com>
2024-11-25 21:31:37 +0700

    Add yolo

--- pages/checkin.py
+++ pages/checkin.py
@@ -4,23 +4,26 @@ from PIL import Image
 
 
 def page():
-    st.title("Checkin")
-    picture = st.camera_input("Camera")
-    model = st.session_state["model"]
-    confidence = st.session_state["confidence"]
-    if picture is not None:
-        img = Image.open(picture)
-        img_array = np.array(img)
-        res = model.predict(img_array, conf=confidence)
-        if len(res[0].boxes) > 0:
-            st.success("Đã checkin thành công!")
-        else:
-            st.error("Không tìm thấy mặt trong ảnh. Vui lòng thử lại!")
+    try:
+        st.title("Checkin")
+        picture = st.camera_input("Camera")
+        model = st.session_state["model"]
+        confidence = st.session_state["confidence"]
+        if picture is not None:
+            img = Image.open(picture)
+            img_array = np.array(img)
+            res = model.predict(img_array, conf=confidence)
+            if len(res[0].boxes) > 0:
+                st.success("Đã checkin thành công!")
+            else:
+                st.error("Không tìm thấy mặt trong ảnh. Vui lòng thử lại!")
 
-    send = st.button("Gửi", type="primary", use_container_width=True)
+        send = st.button("Gửi", type="primary", use_container_width=True)
 
-    if send:
-        st.image(picture)
+        if send:
+            st.image(picture)
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
 
 
 page()
